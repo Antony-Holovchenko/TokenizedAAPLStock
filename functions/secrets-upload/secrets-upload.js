e7a561da00672efb4688591b671128245abb6906
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-07-19 16:00:00 +0400

    Configure a script for secrets upload to DON

--- functions/secrets-upload/secrets-upload.js
+++ functions/secrets-upload/secrets-upload.js
@@ -1,1 +1,22 @@
 const { ethers } = require("hardhat")
+const { SecretsManager } = require("@chainlink/functions-toolkit")
+
+async function uploadSecrets() {
+    // Create a provider and signer instances.
+    const provider = new JsonRpcProvider(process.env.SEPOLIA_RPC_URL)
+    const signer = new Wallet(process.env.PRIVATE_KEY, provider)
+    // Create 
+    const functionsRouterAddress = process.env.SEPOLIA_FUNCTIONS_ROUTER.toString()
+    const donId = process.env.SEPOLIA_DON_ID.toString()
+
+    const secretsManager = new SecretsManager({
+        signer,
+        functionsRouterAddress,
+        donId
+    })
+}
+
+uploadSecrets().catch((error) => {
+    console.error(error)
+    process.exit.code = 1
+})
