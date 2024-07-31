6fb9a5ec156c8503e24cff69c93528f9405b2b58
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-07-31 12:27:00 +0200

    Update deployTKAAP contract + script

--- deploy/01-deployTKAAPL.js
+++ deploy/01-deployTKAAPL.js
@@ -1,26 +1,27 @@
-const { ethers, network } = require("hardhat")
-const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config.js")
+const { network } = require("hardhat")
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
 const fs = require("fs")
 
-module.exports = async({deployments})  => {
+module.exports = async({deployments, getNamedAccounts})  => {
     const {deploy, log} = deployments
-    const [deployer] = await ethers.getSigners();
-
-    const mintSourceCode = fs.readFileSync("./functions/sources/alpaca-balance-request.js", "utf8")
+    const {deployer} = await getNamedAccounts()
+    console.log(deployer);
+    
+    const mintSourceCode = JSON.stringify(fs.readFileSync("./functions/sources/alpaca-balance-request.js", "utf8"))
     const sellSourceCode = ""
 
-    log(`\n============ Deploying to ${network.name}  network ============\n`)
+    log(`\n============ Deploying to ${network.name} network ============\n`)
 
     const deployTKAAPL = await deploy("deployTKAAPL", {
-        deployer: deployer,
+        from: deployer,
         log: true,
         args: [mintSourceCode, sellSourceCode],
-        blockConfirmations: developmentChains.insludes(network.name) 
-        ? 1 
+        blockConfirmations: developmentChains.includes(network.name)
+        ? 1
         : VERIFICATION_BLOCK_CONFIRMATIONS
     })
 
-    log(`\n============ Successfully deployed contract to: ${await deployTKAAPL.address} ============\n`)
+    log(`\n============ Successfully deployed contract to: ${deployTKAAPL.address}  ============\n`)
 }
 
 module.exports.tags = ["deployScript", "all"]
