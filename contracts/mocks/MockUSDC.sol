2acb76efe195e72c16537e58145d6bb9969ea3ef
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-08-03 16:33:40 +0200

    Cahnge testUSDC contract name to MockUSDC

--- contracts/mocks/MockUSDC.sol
+++ contracts/mocks/MockUSDC.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract testUSDC is ERC20 {
-    constructor() ERC20("testUsdc", "TU") {}
+contract MockUSDC is ERC20 {
+    constructor() ERC20("MockUsdc", "MU") {}
 
     function mint() external {
         _mint(msg.sender, 1e6);
