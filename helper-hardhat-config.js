93c0b0d6492f74cba4d1193f4bd43a2e159e56a9
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-08-03 22:45:07 +0200

    Add mock contacts helpers for deployment

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,7 +1,21 @@
 const developmentChains = ["localhost", "hardhat"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-
+const mockContracts = [
+    {
+     name: "MockFunctionsRouter",
+    },
+    {
+     name: "MockUSDC"   
+    },
+    {
+     name: "MockV3AggregatorV1" // contract required for AAPL price feed mock
+    },
+    {
+     name: "MockV3AggregatorV2" // contract required for usdc_price_feed
+    }
+]
 module.exports = {
     developmentChains,
-    VERIFICATION_BLOCK_CONFIRMATIONS
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    mockContracts
 }
